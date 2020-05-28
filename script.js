3cbdbc2b045a5e0f72491757ea54095e7d6bce69
Lia Mun <liacmun@gmail.com>
2020-05-28 10:00:09 -0700

    Added JS, updated HTML & CSS
    created JS and updated HTML AND CSS

---
+++ script.js
@@ -0,0 +1,17 @@
+/*---Variable---*/ 
+const alertBanner = document.getElementById("alert");
+
+/*---Alert Banner*/ 
+alertBanner.innerHTML = 
+    `
+    <div class="alert-banner">
+    <h5><strong>Alert!</strong> Subsribe below to keep updated with new projects. </h5>
+    <p class="alert-close">x</p>
+    </div>
+    `
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-close")) {
+        alertBanner.style.display = "none";
+    }
+});
