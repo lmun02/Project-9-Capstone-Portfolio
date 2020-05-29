ab90a2925d64e49de7e9f8926bd1ce7735df6535
Lia Mun <liacmun@gmail.com>
2020-05-29 10:18:28 -0700

    Updates HTML, CSS & JS
    Add-in profile pic, updated CSS and HTML. Edits to JS

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ alertBanner.innerHTML =
     <h5><strong>Alert!</strong> Subsribe below to keep updated with new projects. </h5>
     <p class="alert-close">x</p>
     </div>
-    `
+    `;
 alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-close")) {
